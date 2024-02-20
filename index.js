61fa76eba780c999f5a0f69afcf4b824d64e1215
Halwest Abubakir <halwest.east@gmail.com>
2024-02-20 13:16:22 +0300

    done

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
 
 // Write your code here
+
+//1st
+const num1 = 31 ;
+const num2 = 2 ;
+
+const multiply = num1 * num2;
+
+console.log(multiply)
+
+//2nd
+const random = Math.floor((Math.random() * 10) + 1);
+
+console.log(random)
+
+//3rd
+const num3 = 20;  
+const num4 = 8;   
+
+
+const mod = (num3 % num4) ;
+
+console.log(mod)
+
+//4th
+const max = Math.max(10, 20)
+console.log(max)
